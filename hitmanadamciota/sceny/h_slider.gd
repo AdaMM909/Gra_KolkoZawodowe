a8b6dbea8dafa0b667c4e4ec066dac8c3169b794
AdaMM909 <adammolka2010@gmail.com>
2026-09-14 12:13:16 +0200

    menu

--- hitmanadamciota/sceny/h_slider.gd
+++ hitmanadamciota/sceny/h_slider.gd
@@ -1,17 +1,15 @@
 extends HSlider
 @export var ktora = ("")
 
-# Called when the node enters the scene tree for the first time.
+var bus_name: String
+var bus_index: int
 
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+	value_changed.connect(_on_value_changed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
-
-
-
-	
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(value)
+	)
