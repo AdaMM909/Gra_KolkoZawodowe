052e9643937ed4a7d5e284c481c2b812a9bcefe9
kita3214 <oliwialadna401@gmail.com>
2026-09-17 00:38:01 +0200

    poprawa guzika reszrta tez do naprawienia

--- hitmanadamciota/sceny/setignsy.gd
+++ hitmanadamciota/sceny/setignsy.gd
@@ -1,9 +1,9 @@
 extends Button
-
-
+var button = self
+@export var gdzie = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pivot_offset = size * Vector2(0.5,1.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,8 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://sceny/settings.tscn")
+	var tween = create_tween()
+	tween.tween_property(button,"scale",Vector2(0.9,1.3),0.2)
+	tween.tween_property(button,"scale",Vector2(1.3,0.9),0.2)
+	await tween.finished
+	get_tree().change_scene_to_file("res://sceny/" + gdzie + ".tscn")
