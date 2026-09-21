a7afc1e331be41903e0b23365cca1a743456b12a
AdaMM909 <adammolka2010@gmail.com>
2026-09-21 12:10:40 +0200

    scena srodku

--- hitmanadamciota/skrypty/character_body_2d.gd
+++ hitmanadamciota/skrypty/character_body_2d.gd
@@ -9,7 +9,7 @@ var predkosc = 400
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if Input.is_action_pressed("ui_down"):
